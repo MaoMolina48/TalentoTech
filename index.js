ad1625c078505f253f8b5993a92306df209c0f89
MaoMolina48 <160797989+MaoMolina48@users.noreply.github.com>
2025-11-26 22:46:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 const userRoutes = require('./Routes/UserRoutes');
 const projectRoutes = require('./Routes/ProjectRoutes');
